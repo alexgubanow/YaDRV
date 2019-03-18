857f137e595f32b256fc86310783a921fa251abf
Oleksandr Hubanov <alexgubanow@ukr.net>
2019-03-18 20:38:10 +0200

    added detailed explanation of all tmc2160 registers

--- stm32f07/tmc2160REGS.h
+++ stm32f07/tmc2160REGS.h
@@ -413,13 +413,15 @@ In this mode, the actual position is determined by the sensorless motor commutat
 motion controller. In case the motor becomes heavily loaded,
 VDCMIN is used as the minimum step velocity.
 Hint: Also set DCCTRL parameters in order to operate DcStep.
-(Only bits 22… 8 are used for value and for comparison)Time reference t for VDCMIN: t = 2^24 / fCLK*/
+(Only bits 22… 8 are used for value and for comparison)
+Time reference t for VDCMIN: t = 2^24 / fCLK*/
 struct VDCMIN_t
 {
 	unsigned int value : 23;
 };
 /*MSLUT 8x 32bit 0 or 1
-reset default= sine wave tableEach bit gives the difference between entry x
+reset default= sine wave table
+Each bit gives the difference between entry x
 and entry x+1 when combined with the corresponding MSLUTSEL W bits:
 0: W= %00: -1
 %01: +0
@@ -440,15 +442,58 @@ struct MSLUT_t
 {
 	unsigned int value[8];
 };
-/*This register defines four segments within
+/*MSLUTSEL – Look up Table Segmentation Definition
+This register defines four segments within
 each quarter MSLUT wave. Four 2 bit entries
 determine the meaning of a 0 and a 1 bit in
 the corresponding segment of MSLUT.
-See separate table!
-0<X1<X2<X3 reset default= sine wave table*/
+0<X1<X2<X3 reset default= sine wave table
+The sine wave look up table can be divided into up to
+four segments using an individual step width control
+entry Wx. The segment borders are selected by X1, X2
+and X3.
+Segment 0 goes from 0 to X1-1.
+Segment 1 goes from X1 to X2-1.
+Segment 2 goes from X2 to X3-1.
+Segment 3 goes from X3 to 255.
+For defined response the values shall satisfy:
+0<X1<X2<X3*/
 struct MSLUTSEL_t
 {
-	unsigned int value;
+	/*Width control bit coding W0…W3:
+%00: MSLUT entry 0, 1 select: -1, +0
+%01: MSLUT entry 0, 1 select: +0, +1
+%10: MSLUT entry 0, 1 select: +1, +2
+%11: MSLUT entry 0, 1 select: +2, +3
+	LUT width select from ofs00 to ofs(X1-1)*/
+	unsigned int W0 : 2;
+	/*Width control bit coding W0…W3:
+%00: MSLUT entry 0, 1 select: -1, +0
+%01: MSLUT entry 0, 1 select: +0, +1
+%10: MSLUT entry 0, 1 select: +1, +2
+%11: MSLUT entry 0, 1 select: +2, +3
+	LUT width select from ofs(X1) to ofs(X2-1)*/
+	unsigned int W1 : 2;
+	/*Width control bit coding W0…W3:
+%00: MSLUT entry 0, 1 select: -1, +0
+%01: MSLUT entry 0, 1 select: +0, +1
+%10: MSLUT entry 0, 1 select: +1, +2
+%11: MSLUT entry 0, 1 select: +2, +3
+	LUT width select from ofs(X2) to ofs(X3-1)*/
+	unsigned int W2 : 2;
+	/*Width control bit coding W0…W3:
+%00: MSLUT entry 0, 1 select: -1, +0
+%01: MSLUT entry 0, 1 select: +0, +1
+%10: MSLUT entry 0, 1 select: +1, +2
+%11: MSLUT entry 0, 1 select: +2, +3
+	LUT width select from xofs(X3) to ofs255*/
+	unsigned int W3 : 2;
+	/*LUT segment 1 start*/
+	unsigned int X1 : 8;
+	/*LUT segment 2 start*/
+	unsigned int X2 : 8;
+	/*LUT segment 3 start*/
+	unsigned int X3 : 8;
 };
 /*START_SIN and START_SIN90 
 Start values are transferred to the microstep
@@ -496,19 +541,158 @@ MSLUT (not scaled by current)
 +/-0...255 */
 	int CUR_B : 9;
 };
-/*chopper and driver configuration
-See separate table!
+/*CHOPCONF – Chopper Configuration
 reset default=0x10410150*/
 struct CHOPCONF_t
 {
-	unsigned int value;
+	/*TOFF off time and driver enable
+	Off time setting controls duration of slow decay phase
+NCLK= 24 + 32*TOFF
+%0000: Driver disable, all bridges off
+%0001: 1 – use only with TBL ≥ 2
+%0010 … %1111: 2 … 15*/
+	unsigned int toff : 4;
+	/*When ChpperMode(chm)=0 this register become to HSTRT
+hysteresis start value added to HEND
+%000 … %111: Add 1, 2, …, 8 to hysteresis low value HEND
+(1/512 of this setting adds to current setting)
+Attention: Effective HEND+HSTRT ≤ 16.
+Hint: Hysteresis decrement is done each 16 clocksWhen ChpperMode(chm)=1 this register become to TFD Fast decay time setting (MSB: fd3):
+%0000 … %1111: Fast decay time setting TFD with
+NCLK= 32*TFD (%0000: slow decay only)*/
+	unsigned int HSTRT_TFD : 3;
+	/*When ChpperMode(chm)=0 this register become to HEND hysteresis low value
+	%0000 … %1111:
+Hysteresis is -3, -2, -1, 0, 1, …, 12
+(1/512 of this setting adds to current setting)
+This is the hysteresis value which becomes
+used for the hysteresis chopper.
+When ChpperMode(chm)=1 this register become to OFFSET sine wave offset
+%0000 … %1111:
+Offset is -3, -2, -1, 0, 1, …, 12
+This is the sine wave offset and 1/512 of the
+value becomes added to the absolute value
+of each sine wave entry.*/
+	unsigned int HEND_OFFSET : 4;
+	/*When ChpperMode(chm) = 1 this register become to MSB of fast decay time
+	setting TFD Fast decay time setting(MSB: fd3) :
+		% 0000 … % 1111 : Fast decay time setting TFD with
+		NCLK = 32 * TFD(% 0000: slow decay only)*/
+	unsigned int TFD3 : 1;
+	/*fast decay mode
+	When ChpperMode(chm) = 1 setting disfdcc=1 disables current comparator usage for termination of the fast decay cycle*/
+	unsigned int disfdcc : 1;
+	unsigned int : 1;
+	/*chopper mode
+	Set 0 - Standard mode (SpreadCycle)
+Set 1 - Constant off time with fast decay time.
+Fast decay time is also terminated when the
+negative nominal current is reached. Fast decay is
+after on time.*/
+	unsigned int chm : 1;
+	/*TBL blank time select %00 … %11: Set comparator blank time to 16, 24, 36 or 54 clocks
+Hint: %01 or %10 is recommended for most applications*/
+	unsigned int tbl : 2;
+	unsigned int : 1;
+	/*high velocity fullstep selection 
+	This bit enables switching to fullstep, when VHIGH is
+exceeded. Switching takes place only at 45° position.
+The fullstep target current uses the current value from
+the microstep table at the 45° position.*/
+	unsigned int vhighfs : 1;
+	/*high velocity chopper mode
+This bit enables switching to chm=1 and fd=0, when VHIGH
+is exceeded. This way, a higher velocity can be achieved.
+Can be combined with vhighfs=1. If set, the TOFF setting
+automatically becomes doubled during high velocity
+operation in order to avoid doubling of the chopper
+frequency.*/
+	unsigned int vhighchm : 1;
+	/*TPFD allows dampening of motor mid-range resonances.
+Passive fast decay time setting controls duration of the
+fast decay phase inserted after bridge polarity change
+NCLK= 128*TPFD
+%0000: Disable
+%0001 … %1111: 1 … 15*/
+	unsigned int TPFD : 4;
+	/*MRES micro step resolution
+	%0000: Native 256 microstep setting. Normally use this setting with the internal motion controller.
+	%0001 … %1000: 128, 64, 32, 16, 8, 4, 2, FULLSTEP
+Reduced microstep resolution esp. for STEP/DIR operation.
+The resolution gives the number of microstep entries per
+sine quarter wave.
+The driver automatically uses microstep positions which
+result in a symmetrical wave, when choosing a lower
+microstep resolution.
+step width=2^MRES [microsteps]*/
+	unsigned int MRES : 4;
+	/*interpolation to 256 microsteps
+	1: The actual microstep resolution (MRES) becomes
+extrapolated to 256 microsteps for smoothest motor
+operation (useful for STEP/DIR operation, only)*/
+	unsigned int intpol : 1;
+	/*enable double edge step pulses
+	1: Enable step impulse at each step edge to reduce step
+frequency requirement. */
+	unsigned int dedge : 1;
+	/*short to GND protection disable
+	0: Short to GND protection is on
+1: Short to GND protection is disabled*/
+	unsigned int diss2g : 1;
+	/*short to supply protection disable
+	0: Short to VS protection is on
+1: Short to VS protection is disabled*/
+	unsigned int diss2vs : 1;
 };
-/*CoolStep smart current control register
-and StallGuard2 configuration
-See separate table!*/
+/*COOLCONF – Smart Energy Control CoolStep and StallGuard2*/
 struct COOLCONF_t
 {
-	unsigned int value : 25;
+	/*minimum StallGuard2 value for smart current control and smart current enable
+	If the StallGuard2 result falls below SEMIN*32, the motor
+current becomes increased to reduce motor load angle.
+%0000: smart current control CoolStep off
+%0001 … %1111: 1 … 15*/
+	unsigned int semin : 4;
+	unsigned int : 1;
+	/*current up step width 
+	Current increment steps per measured StallGuard2 value
+	%00 … %11: 1, 2, 4, 8*/
+	unsigned int seup : 2;
+	unsigned int : 1;
+	/*StallGuard2 hysteresis value for smart current control
+If the StallGuard2 result is equal to or above
+(SEMIN+SEMAX+1)*32, the motor current becomes
+decreased to save energy.
+%0000 … %1111: 0 … 15*/
+	unsigned int semax : 4;
+	unsigned int : 1;
+	/*current down step speed
+	%00: For each 32 StallGuard2 values decrease by one
+%01: For each 8 StallGuard2 values decrease by one
+%10: For each 2 StallGuard2 values decrease by one
+%11: For each StallGuard2 value decrease by one*/
+	unsigned int sedn : 2;
+	/*minimum current for smart current control
+0: 1/2 of current setting (IRUN)
+1: 1/4 of current setting (IRUN)*/
+	unsigned int seimin : 1;
+	/*StallGuard2 threshold value
+	This signed value controls StallGuard2 level for stall
+output and sets the optimum measurement range for
+readout. A lower value gives a higher sensitivity. Zero is
+the starting value working with most motors.
+-64 to +63: A higher value makes StallGuard2 less
+sensitive and requires more torque to
+indicate a stall.*/
+	int sgt : 7;
+	unsigned int : 1;
+	/*StallGuard2 filter enable
+	Set 0 - Standard mode, high time resolution for StallGuard2
+	Set 1 - Filtered mode, StallGuard2 signal updated for each
+four fullsteps (resp. six fullsteps for 3 phase motor)
+only to compensate for motor pole tolerances*/
+	unsigned int sfilt : 1;
+	unsigned int : 1;
 };
 /*DcStep (DC) automatic commutation
 configuration register (enable via pin DCEN
@@ -535,18 +719,191 @@ StallGuard2 in DcStep mode.
 Set slightly higher than DC_TIME/16 0=disable*/
 	unsigned int DC_SG : 8;
 };
-/*StallGuard2 value and driver error flags
-See separate table!*/
+/*DRV_STATUS – StallGuard2 Value and Driver Error Flags*/
 struct DRV_STATUS_t
 {
-	unsigned int value;
+	/*StallGuard2 result respectively PWM on time for coil A in stand still for motor temperature detection
+	Mechanical load measurement:
+The StallGuard2 result gives a means to measure mechanical
+motor load. A higher value means lower mechanical load. A
+value of 0 signals highest load. With optimum SGT setting,
+this is an indicator for a motor stall. The stall detection
+compares SG_RESULT to 0 in order to detect a stall. SG_RESULT
+is used as a base for CoolStep operation, by comparing it to a
+programmable upper and a lower limit. It is not applicable in
+StealthChop mode.
+StallGuard2 works best with microstep operation or DcStep.
+Temperature measurement:
+In standstill, no StallGuard2 result can be obtained. SG_RESULT
+shows the chopper on-time for motor coil A instead. Move the
+motor to a determined microstep position at a certain current
+setting to get a rough estimation of motor temperature by a
+reading the chopper on-time. As the motor heats up, its coil
+resistance rises and the chopper on-time increases. */
+	unsigned int SG_RESULT : 10;
+	unsigned int : 2;
+	/*short to supply indicator phase A
+	1: Short to supply detected on phase A or B. The driver
+becomes disabled. The flags stay active, until the driver is
+disabled by software (TOFF=0) or by the ENN input. Sense
+resistor voltage drop is included in the measurement!*/
+	unsigned int s2vsa : 1;
+	/*short to supply indicator phase B
+	1: Short to supply detected on phase A or B. The driver
+becomes disabled. The flags stay active, until the driver is
+disabled by software (TOFF=0) or by the ENN input. Sense
+resistor voltage drop is included in the measurement!*/
+	unsigned int s2vsb : 1;
+	/*StealthChop indicator 
+	1: Driver operates in StealthChop mode*/
+	unsigned int stealth : 1;
+	/*full step active indicator
+1: Indicates that the driver has switched to fullstep as defined
+by chopper mode settings and velocity thresholds. */
+	unsigned int fsactive : 1;
+	/*actual motor current / smart energy current
+Actual current control scaling, for monitoring smart energy
+current scaling controlled via settings in register COOLCONF, or
+for monitoring the function of the automatic current scaling.*/
+	unsigned int CS_ACTUAL : 5;
+	unsigned int : 3;
+	/*StallGuard2 status 1: Motor stall detected (SG_RESULT=0) or DcStep stall in DcStep
+mode.*/
+	unsigned int stallGuard : 1;
+	/*overtemperature flag 1: Overtemperature limit has been reached. Drivers become
+disabled until otpw is also cleared due to cooling down of the IC. 
+The overtemperature flag is common for both bridges.*/
+	unsigned int ot : 1;
+	/*overtemperature prewarning flag
+1: Overtemperature pre-warning threshold is exceeded.
+The overtemperature pre-warning flag is common for both
+bridges.*/
+	unsigned int otpw : 1;
+	/*short to ground indicator phase A
+	1: Short to GND detected on phase A or B. The driver becomes
+disabled. The flags stay active, until the driver is disabled by
+software (TOFF=0) or by the ENN input.*/
+	unsigned int s2ga : 1;
+	/*short to ground indicator phase B
+	1: Short to GND detected on phase A or B. The driver becomes
+disabled. The flags stay active, until the driver is disabled by
+software (TOFF=0) or by the ENN input.*/
+	unsigned int s2gb : 1;
+	/*open load indicator phase A
+	1: Open load detected on phase A or B.
+Hint: This is just an informative flag. The driver takes no action
+upon it. False detection may occur in fast motion and
+standstill. Check during slow motion, only.*/
+	unsigned int ola : 1;
+	/*open load indicator phase A
+	1: Open load detected on phase A or B.
+Hint: This is just an informative flag. The driver takes no action
+upon it. False detection may occur in fast motion and
+standstill. Check during slow motion, only.*/
+	unsigned int olb : 1;
+	/*standstill indicator This flag indicates motor stand still in each operation mode.
+This occurs 2^20 clocks after the last step pulse.*/
+	unsigned int stst : 1;
 };
-/*Voltage PWM mode chopper configuration
-See separate table!
+/*PWMCONF – Voltage PWM Mode StealthChop
 reset default=0xC40C001E*/
 struct PWMCONF_t
 {
-	unsigned int value;
+	/*PWM_OFS User defined amplitude(offset)
+	User defined PWM amplitude offset (0-255) related to full
+motor current (CS_ACTUAL=31) in stand still.(Reset default=30)
+Use PWM_OFS as initial value for automatic scaling to
+speed up the automatic tuning process. To do this, set
+PWM_OFS to the determined, application specific value,
+with pwm_autoscale=0. Only afterwards, set
+pwm_autoscale=1. Enable StealthChop when finished.
+PWM_OFS = 0 will disable scaling down motor current
+below a motor specific lower measurement threshold.
+This setting should only be used under certain
+conditions, i.e. when the power supply voltage can vary
+up and down by a factor of two or more. It prevents
+the motor going out of regulation, but it also prevents
+power down below the regulation limit.
+PWM_OFS > 0 allows automatic scaling to low PWM duty
+cycles even below the lower regulation threshold. This
+allows low (standstill) current settings based on the
+actual (hold) current scale (register IHOLD_IRUN). */
+	unsigned int PWM_OFS : 8;
+	/*PWM_GRAD User defined amplitude gradient
+	Velocity dependent gradient for PWM amplitude: PWM_GRAD * 256 / TSTEP
+This value is added to PWM_OFS to compensate for the
+velocity-dependent motor back-EMF.
+Use PWM_GRAD as initial value for automatic scaling to
+speed up the automatic tuning process. To do this, set
+PWM_GRAD to the determined, application specific value,
+with pwm_autoscale=0. Only afterwards, set
+pwm_autoscale=1. Enable StealthChop when finished.
+Hint: After initial tuning, the required initial value can be read
+out from PWM_GRAD_AUTO.*/
+	unsigned int PWM_GRAD : 8;
+	/*PWM frequency selection 
+	%00: fPWM=2/1024 fCLK (Reset default)
+%01: fPWM=2/683 fCLK
+%10: fPWM=2/512 fCLK
+%11: fPWM=2/410 fCLK*/
+	unsigned int pwm_freq : 2;
+	/*PWM automatic amplitude scaling
+Set 0 - User defined feed forward PWM amplitude. The
+current settings IRUN and IHOLD have no influence!
+The resulting PWM amplitude (limited to 0…255) is:
+PWM_OFS * ((CS_ACTUAL+1) / 32)
++ PWM_GRAD * 256 / TSTEP
+Set 1 - Enable automatic current control (Reset default)*/
+	unsigned int pwm_autoscale : 1;
+	/*PWM automatic gradient adaptation
+	Set to 0 Fixed value for PWM_GRAD (PWM_GRAD_AUTO = PWM_GRAD)
+Set to 1 Automatic tuning (only with pwm_autoscale=1)
+(Reset default)
+PWM_GRAD_AUTO is initialized with PWM_GRAD
+while pwm_autograd=0 and becomes optimized
+automatically during motion.
+Preconditions
+1. PWM_OFS_AUTO has been automatically
+initialized. This requires standstill at IRUN for
+>130ms in order to a) detect standstill b) wait >
+128 chopper cycles at IRUN and c) regulate
+PWM_OFS_AUTO so that
+-1 < PWM_SCALE_AUTO < 1
+2. Motor running and 1.5 * PWM_OFS_AUTO <
+PWM_SCALE_SUM < 4* PWM_OFS_AUTO and
+PWM_SCALE_SUM < 255.
+Time required for tuning PWM_GRAD_AUTO
+About 8 fullsteps per change of +/-1.
+Also enables use of reduced chopper frequency for
+tuning PWM_OFS_AUTO.*/
+	unsigned int pwm_autograd : 1;
+	/*Allows different standstill modes
+	Stand still option when motor current setting is zero (I_HOLD=0).
+%00: Normal operation
+%01: Freewheeling
+%10: Coil shorted using LS drivers
+%11: Coil shorted using HS drivers*/
+	unsigned int freewheel : 2;
+	unsigned int : 2;
+	/*Regulation loop gradient
+	User defined maximum PWM amplitude change per half wave when using pwm_autoscale=1. (1…15):
+1: 0.5 increments (slowest regulation)
+2: 1 increment
+3: 1.5 increments
+4: 2 increments (Reset default))
+...
+8: 4 increments
+...
+15: 7.5 increments (fastest regulation)*/
+	unsigned int PWM_REG : 4;
+	/*PWM automatic scale amplitude limit when switching onLimit for PWM_SCALE_AUTO when switching back from
+SpreadCycle to StealthChop. This value defines the upper
+limit for bits 7 to 4 of the automatic current control
+when switching back. It can be set to reduce the current
+jerk during mode change back to StealthChop.
+It does not limit PWM_GRAD or PWM_GRAD_AUTO offset.
+(Default = 12)*/
+	unsigned int PWM_LIM : 4;
 };
 /*Results of StealthChop amplitude regulator.
 These values can be used to monitor
