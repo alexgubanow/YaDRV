9106b7c1ee27e62b799b520fd6c1a801ca5002e4
Oleksandr Hubanov <alexgubanow@ukr.net>
2019-04-03 09:34:52 +0300

    new code from cubemx

--- Core/Src/stm32f0xx_it.c
+++ Core/Src/stm32f0xx_it.c
@@ -71,10 +71,10 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
+extern DMA_HandleTypeDef hdma_spi1_rx;
+extern DMA_HandleTypeDef hdma_spi1_tx;
 extern SPI_HandleTypeDef hspi1;
 extern UART_HandleTypeDef huart1;
-extern TIM_HandleTypeDef htim17;
-
 /* USER CODE BEGIN EV */
 
 /* USER CODE END EV */
@@ -144,7 +144,7 @@ void SysTick_Handler(void)
   /* USER CODE BEGIN SysTick_IRQn 0 */
 
   /* USER CODE END SysTick_IRQn 0 */
-  
+  HAL_IncTick();
   /* USER CODE BEGIN SysTick_IRQn 1 */
 
   /* USER CODE END SysTick_IRQn 1 */
@@ -173,17 +173,18 @@ void EXTI0_1_IRQHandler(void)
 }
 
 /**
-  * @brief This function handles TIM17 global interrupt.
+  * @brief This function handles DMA1 channel 2 and 3 interrupts.
   */
-void TIM17_IRQHandler(void)
+void DMA1_Channel2_3_IRQHandler(void)
 {
-  /* USER CODE BEGIN TIM17_IRQn 0 */
+  /* USER CODE BEGIN DMA1_Channel2_3_IRQn 0 */
 
-  /* USER CODE END TIM17_IRQn 0 */
-  HAL_TIM_IRQHandler(&htim17);
-  /* USER CODE BEGIN TIM17_IRQn 1 */
+  /* USER CODE END DMA1_Channel2_3_IRQn 0 */
+  HAL_DMA_IRQHandler(&hdma_spi1_rx);
+  HAL_DMA_IRQHandler(&hdma_spi1_tx);
+  /* USER CODE BEGIN DMA1_Channel2_3_IRQn 1 */
 
-  /* USER CODE END TIM17_IRQn 1 */
+  /* USER CODE END DMA1_Channel2_3_IRQn 1 */
 }
 
 /**
