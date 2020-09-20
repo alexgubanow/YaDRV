f788d5d6f85a804a89d669f21edbb65fd19be312
Oleksandr Hubanov <alex@0x0h.com>
2020-09-20 15:40:27 +0300

    add usbhid and tmc controls

--- TuneYaDRV/ViewModels/MainWindowViewModel.cs
+++ TuneYaDRV/ViewModels/MainWindowViewModel.cs
@@ -4,7 +4,7 @@ namespace TuneYaDRV.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
+        private string _title = "Tune YaDRV";
         public string Title
         {
             get { return _title; }
--- TuneYaDRV/Views/MainWindow.xaml.cs
+++ TuneYaDRV/Views/MainWindow.xaml.cs
@@ -1,15 +1,33 @@
 ﻿using System.Windows;
+using Prism.Ioc;
+using Prism.Events;
+using Prism.Regions;
+using MahApps.Metro.Controls;
+using TMCRegisterControl;
 
 namespace TuneYaDRV.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
-        public MainWindow()
+        private readonly IContainerExtension _container;
+        private readonly IRegionManager _regionManager;
+
+        private readonly IEventAggregator _ea;
+        public MainWindow(IContainerExtension container, IRegionManager regionManager, IEventAggregator ea)
         {
             InitializeComponent();
+            _container = container;
+            _regionManager = regionManager;
+            _ea = ea;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _regionManager.Regions["UsbHIDControlRegion"].Add(_container.Resolve<UsbHIDControl>());
+            _regionManager.Regions["TMC2590ControlRegion"].Add(_container.Resolve<TMC2590>());
         }
     }
 }
